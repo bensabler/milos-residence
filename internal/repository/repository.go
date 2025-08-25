8efd436efedd13dabb041709c35bd4b614ed2a28
Ben Sabler <bensabler@protonmail.com>
2025-08-24 20:45:54 -0400

    added some basic db query functions

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,15 @@
 package repository
 
-import "github.com/bensabler/milos-residence/internal/models"
+import (
+	"time"
+
+	"github.com/bensabler/milos-residence/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
