f48ad4e5e17dbc9a5e27f1805017662407ba3405
Ben Sabler <bensabler@protonmail.com>
2025-08-24 21:36:48 -0400

    added more db functions

--- migrations/20250824231010_seed_restrictions_reservation.sql
+++ migrations/20250824231010_seed_restrictions_reservation.sql
@@ -1,14 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
 INSERT INTO restrictions (restriction_name)
-SELECT 'Reservation'
+SELECT v.name
+FROM (VALUES ('Reservation'), ('Owner Block')) AS v(name)
 WHERE NOT EXISTS (
-  SELECT 1 FROM restrictions WHERE restriction_name = 'Reservation'
+  SELECT 1 FROM restrictions r WHERE r.restriction_name = v.name
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DELETE FROM restrictions
-WHERE restriction_name = 'Reservation';
+WHERE restriction_name IN ('Reservation','Owner Block');
 -- +goose StatementEnd
